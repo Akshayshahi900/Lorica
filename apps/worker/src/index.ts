67d7c065d4c1783782abd71265988bb15ec199cd
Akshay shahi <shahiakshay900@gmail.com>
2026-09-16 23:13:14 +0530

    Feat:Add health endpoint in worker

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,7 +1,13 @@
 import "dotenv/config";
 import './codeIndex.worker';
 import './diff.worker';
+import express from "express";
 
+const health = express();
+health.get("/health", (_req, res) => res.send("OK"));
+health.listen(process.env.PORT || 10000, () => {
+  console.log("worker health endpoint listening");
+});
 console.log('[lorica:worker] All workers booted, waiting for jobs...');
 
 process.on('SIGTERM', () => {
